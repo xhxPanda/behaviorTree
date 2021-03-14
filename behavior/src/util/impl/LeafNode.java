954f607ca47c9415915b916c83f662adcd11a74a
XieHongXin <171510228@qq.com>
2021-03-14 18:09:12 +0800

    （行为树）：修改基本架构

---
+++ behavior/src/util/impl/LeafNode.java
@@ -0,0 +1,36 @@
+package util.impl;
+
+import command.ResultCode;
+import util.AbstractNode;
+
+public class LeafNode extends AbstractNode {
+    @Override
+    public int addChildIndex(AbstractNode node) {
+        return 0;
+    }
+
+    @Override
+    public int getChildrenNum() {
+        return 0;
+    }
+
+    @Override
+    public boolean isSuccess(AbstractNode parent) {
+        return false;
+    }
+
+    @Override
+    public ResultCode tick() {
+        return null;
+    }
+
+    @Override
+    public void onInitialize() {
+
+    }
+
+    @Override
+    public ResultCode action() {
+        return null;
+    }
+}
